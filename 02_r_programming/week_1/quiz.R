abf6c6f4dc0cb61638ee518453242eb23ffcb915
godfrey_workpc <oyasor@gmail.com>
2015-07-16 17:22:02 +0100

    swirl

--- 02_r_programming/week_1/quiz.R
+++ 02_r_programming/week_1/quiz.R
@@ -1,19 +1,44 @@
 # Quiz 1
 quizData <- read.csv("C:\\USers\\goyasor\\coursera\\datasciencecoursera\\02_r_programming\\week_1\\quiz1_data\\hw1_data.csv", header = TRUE)
 
+#11 In the dataset provided for this Quiz, what are the column names of the dataset?
+names(quizData)
+
+#12 Extract the first 2 rows of the data frame and print them to the console. What does the output look like?
+head(quizData, 2)
+
+#13 How many observations (i.e. rows) are in this data frame?
+nrow(quizData)
+
+#14 Extract the last 2 rows of the data frame and print them to the console. What does the output look like?
+tail(quizData, 2)
+
+#15 What is the value of Ozone in the 47th row?
+quizData[47,"Ozone"]
+
+#16 How many missing values are in the Ozone column of this data frame?
+sum(is.na(quizData["Ozone"]))
+
+#17 What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
+naOzone <- is.na(quizData[,"Ozone"])
+ozone <- quizData[,"Ozone"]
+ozone[!naOzone]
+mean(ozone[!naOzone])
+sum(ozone[!naOzone])
+
+#18 Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. What is the mean of Solar.R in this subset?
+subset(quizData, Ozone>31 & Temp>90, select = Solar.R)
+subsetSolar.R <- subset(quizData, Ozone>31 & Temp>90, select = Solar.R)
+mean(subsetSolar.R[, "Solar.R"])
+
+#19 What is the mean of "Temp" when "Month" is equal to 6? 
+subsetMonth <- subset(quizData, Month==6, select = c(Month, Temp))
+subsetMonth
+mean(subsetMonth[, "Temp"])
+
+#20 What was the maximum ozone value in the month of May (i.e. Month = 5)?
+subsetOzone <- subset(quizData, Month==5, select = c(Month, Ozone))
+subsetOzone
+max(subsetOzone[,"Ozone"], na.rm = TRUE)
 
 rm(list = ls())
-
-x <- c(1:4)
-mean(x)
-
-y <- c(1, 2, 3, NA)
-good <- complete.cases(y)
-y <- y[good]
-mean(y)
-y
-
-ozone <- quizData["Ozone"]
-class(ozone)
-good <- complete.cases(ozone)
-ozone <- ozone[good]
---
+++ 02_r_programming/week_1/swirl.R
@@ -0,0 +1,25 @@
+# SWIRL -> Statistics With Interactive R Learning
+install.packages("swirl")
+library("swirl")
+swirl()
+
+# Exit swirl and return to the R prompt (>) at any time by pressing the Esc key
+# If you are already at the prompt, type bye() to exit and save your progress.
+
+# When you are at the R prompt (>):
+# Typing skip() allows you to skip the current question.
+# Typing play() lets you experiment with R on your own; swirl will ignore what you do...
+# UNTIL you type nxt() which will regain swirl's attention.
+# Typing bye() causes swirl to exit. Your progress will be saved.
+# Typing main() returns you to swirl's main menu.
+# Typing info() displays these options again.
+
+# To begin, you must install a course. I can install a course for you from the internet, or I can send you to a web page
+# (https://github.com/swirldev/swirl_courses) which will provide course options and directions for installing courses yourself.
+# (If you are not connected to the internet, type 0 to exit.)
+
+# If at any point you'd like more information on a particular topic related to R, you can type help.start() at the prompt, which
+# will open a menu of resources (either within RStudio or your default web browser, depending on your setup). Alternatively, a
+# simple web search often yields the answer you're looking for.
+
+rm(list = ls())
